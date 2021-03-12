84a5d55d04227bbed94837a67dec7a685ba2f752
Erik <erik.antonio@raizen.com>
2021-03-11 21:05:26 -0300

    DOGS003 - Adicionado NavigationGraph

--- app/src/main/java/com/unicep/app/dogs/view/MainActivity.kt
+++ app/src/main/java/com/unicep/app/dogs/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.unicep.app.dogs
+package com.unicep.app.dogs.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.unicep.app.dogs.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
