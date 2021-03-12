a1adab98176f799002f6ac335f76257aab24905e
Erik <erik.antonio@raizen.com>
2021-03-11 22:05:56 -0300

    DOGS003 - Adicionado Kotlin extensions e binding

--- app/src/main/java/com/unicep/app/dogs/view/ListFragment.kt
+++ app/src/main/java/com/unicep/app/dogs/view/ListFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.unicep.app.dogs.R
+import kotlinx.android.synthetic.main.fragment_list.*
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -26,6 +27,7 @@ class ListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // TODO: continuar daqui
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
