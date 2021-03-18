036f0837fdec4f5e46f03be43e7c7b28aa44ebae
Erik <erik.antonio@raizen.com>
2021-03-18 20:11:27 -0300

    DOGS003 - Adicionado action bar

--- app/src/main/java/com/unicep/app/dogs/view/MainActivity.kt
+++ app/src/main/java/com/unicep/app/dogs/view/MainActivity.kt
@@ -2,11 +2,22 @@ package com.unicep.app.dogs.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.unicep.app.dogs.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var _navigation: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        _navigation = Navigation.findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, _navigation)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(_navigation, null)
     }
 }
