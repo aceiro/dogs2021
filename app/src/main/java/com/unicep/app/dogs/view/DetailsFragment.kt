e3a74cb5130c8909d9ab89d21d1084913702dbb3
Erik <erik.antonio@raizen.com>
2021-03-18 21:35:42 -0300

    DOGS003 - Aidicionado passagem de parametros

--- app/src/main/java/com/unicep/app/dogs/view/DetailsFragment.kt
+++ app/src/main/java/com/unicep/app/dogs/view/DetailsFragment.kt
@@ -5,31 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.unicep.app.dogs.R
 import kotlinx.android.synthetic.main.fragment_details.*
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 class DetailsFragment : Fragment() {
-    private val _message = "Click on DetailsFragment:: button"
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
         return inflater.inflate(R.layout.fragment_details, container, false)
     }
@@ -37,25 +22,17 @@ class DetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        arguments?.let {
+            val message = DetailsFragmentArgs.fromBundle(it).message
+            tvDetailsMessage.text = message
+
+        }
+
         floatingActionDetailsButton.setOnClickListener {
-
-            Toast
-                .makeText(requireContext(), _message, Toast.LENGTH_SHORT)
-                .show()
-
-            val action = DetailsFragmentDirections.actionDetailsFragmentToListFragment()
+            val uuid = 100
+            val action = DetailsFragmentDirections.actionListFragment(uuid)
             Navigation.findNavController(it).navigate(action)
         }
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DetailsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
--- app/src/main/java/com/unicep/app/dogs/view/ListFragment.kt
+++ app/src/main/java/com/unicep/app/dogs/view/ListFragment.kt
@@ -4,31 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
+import android.widget.Toast.makeText
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.unicep.app.dogs.R
 import kotlinx.android.synthetic.main.fragment_list.*
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 class ListFragment : Fragment() {
-    private val _message = "Click on ListFragment:: button"
-    private var _param1: String? = null
-    private var _param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            _param1 = it.getString(ARG_PARAM1)
-            _param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val _message = "Recebido o valor de número 100!"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
 
         super.onCreateView(inflater, container, savedInstanceState)
@@ -38,30 +27,20 @@ class ListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        floatingActionListButton.setOnClickListener {
+        var uuid = -1
+        arguments?.let {
+            uuid = ListFragmentArgs.fromBundle(it).uuid
+            tvListMessage.text = getString(R.string.message_received, uuid)
+        }
 
-            // 1. shows Toast
-            Toast
-                .makeText(this.requireContext(), _message, Toast.LENGTH_SHORT)
-                .show()
+        if (uuid == 100) {
+            makeText(this.requireContext(), _message, LENGTH_SHORT).show()
+        }
 
-            // 2. open new Fragment (details)
-            val action = ListFragmentDirections.actionListFragmentToDetailsFragment()
+        btnList.setOnClickListener {
+            val action = ListFragmentDirections.actionDetailsFragment(_message)
             Navigation.findNavController(it).navigate(action)
 
         }
-    }
-
-
-
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
